e7cbde93fa097eba1ea429f8cacae6343f3c82d9
ADR-projects <i.adrijadas@gmail.com>
2025-09-24 21:23:47 +0530

    Initialise components, import assets

---
+++ src/assets/assets.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import homeIcon from './home.svg';
+import searchIcon from './search.svg';
+import videoIcon from './video.svg';
+import plusIcon from './plus.svg';
+
+import imageIcon from './image.svg';
+import profileIcon from './profile.svg';
+import logoutIcon from './logout.svg';
+import heartIcon from './heart.svg';
+import commentIcon from './comment.svg';
+import shareIcon from './share.svg';
+import bookmarkIcon from './bookmark.svg';
+
+
+export const assets = {
+  logo,
+  homeIcon,
+  searchIcon,
+  plusIcon,
+  imageIcon,
+  videoIcon,
+  profileIcon,
+  logoutIcon,
+  heartIcon,
+  commentIcon,
+  shareIcon,
+  bookmarkIcon
+};
