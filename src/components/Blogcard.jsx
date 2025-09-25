7f01fe0a003e0036488bfb2647c2bea773f46b44
ADR-projects <i.adrijadas@gmail.com>
2025-09-25 20:02:20 +0530

    Update Assets

--- src/components/Blogcard.jsx
+++ src/components/Blogcard.jsx
@@ -7,7 +7,7 @@ const Blogcard = ({blog}) => {
     const navigate = useNavigate()
 
     return (
-        <div onClick={()=>navigate(`/blog/${_id}`)} className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-secondary/25 duration-300 cursor-pointer'>
+        <div onClick={()=>navigate(`/blog/${_id}`)} className='w-full rounded-lg bg-ternary/50 overflow-hidden shadow hover:scale-102 hover:shadow-secondary/25 duration-300 cursor-pointer'>
             <img src={image} alt="image" className='aspect-auto' />
             <span className='ml-5 mt-4 px-3 py-1 inline-block bg-secondary/20 rounded-full text-primary text-xs'>
                 {category}
